c9354774707d08cf00010ceebe2d8183e4c435c3
Zachary Anker <zach.anker@gmail.com>
2013-08-05 22:24:19 -0700

    Be lazy and specify method and quality method and quality for now, will move these to configs later

--- lib/sprockets/webp/converter.rb
+++ lib/sprockets/webp/converter.rb
@@ -30,7 +30,7 @@ module Sprockets
           file.close
 
           # Encode Original File Temp copy to WebP File Pathname
-          ::WebP.encode(file.path, webp_path.to_path)
+          ::WebP.encode(file.path, webp_path.to_path, :method => 6, :quality => 92)
         end
 
         data
